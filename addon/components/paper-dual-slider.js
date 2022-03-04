26d1ee96e9c9eed8df609620d2211333f9aaa15e
Jimmy <ultish@gmail.com>
2022-03-04 14:25:54 +1100

    upgrade for latest ember-paper

--- addon/components/paper-dual-slider.js
+++ addon/components/paper-dual-slider.js
@@ -1,66 +1,67 @@
-import { computed } from "@ember/object";
-import { htmlSafe } from "@ember/string";
-import layout from "../templates/components/paper-dual-slider";
-
-import FocusableMixin from "ember-paper/mixins/focusable-mixin";
-import ColorMixin from "ember-paper/mixins/color-mixin";
+import PaperSlider from 'ember-paper/components/paper-slider/component';
+import {tracked} from '@glimmer/tracking';
+import {htmlSafe} from "@ember/string";
 import clamp from "ember-paper/utils/clamp";
-import Component from "ember-paper/components/paper-slider";
 
-const percentStyleComputed = key =>
-  computed(key, function() {
-    const percent = this.get(key);
-    return htmlSafe(`left: ${(percent || 0) * 100}%;`);
-  });
+export default class PaperDualSliderComponent extends PaperSlider {
 
-const percentComputed = key =>
-  computed(key, "min", "max", function() {
-    const value = this.get(key);
-    const min = parseFloat(this.get("min") || 0, 10);
-    const max = parseFloat(this.get("max") || 0, 10);
+  @tracked from;
+  @tracked to;
+  @tracked min;
+  @tracked max;
+  onChange = undefined;
 
+  constructor(owner, args) {
+    super(owner, args);
+    const {from, to, min, max, onChange} = args;
+    this.from = from;
+    this.to = to;
+    this.min = min;
+    this.max = max;
+    this.onChange = onChange;
+  }
+
+  calcPercent(key) {
+    const value = key
+    const min = this.min ? parseFloat(this.min) : 0.0;
+    const max = this.max ? parseFloat(this.max) : 10.0;
     return clamp((value - min) / (max - min), 0, 1);
-  });
+  }
 
-export default Component.extend(FocusableMixin, ColorMixin, {
-  layout,
-  leftPercent: percentComputed("from"),
-  rightPercent: percentComputed("to"),
+  calcPercentStyle(key) {
+    return htmlSafe(`left: ${(key || 0) * 100}%`);
+  }
 
-  leftThumbStyle: percentStyleComputed("leftPercent"),
-  rightThumbStyle: percentStyleComputed("rightPercent"),
+  get leftPercent() {
+    return this.calcPercent(this.from);
+  }
 
-  activeTrackStyle: computed("leftPercent", "rightPercent", function() {
-    let left = this.get("leftPercent");
-    let right = this.get("rightPercent");
-    left *= 100;
-    right *= 100;
+  get rightPercent() {
+    return this.calcPercent(this.to);
+  }
+
+  get leftThumbStyle() {
+    return this.calcPercentStyle(this.leftPercent);
+  }
+
+  get rightThumbStyle() {
+    return this.calcPercentStyle(this.rightPercent);
+  }
+
+  get activeTrackStyle() {
+    const left = this.leftPercent * 100;
+    const right = this.rightPercent * 100;
     return htmlSafe(`left: ${left}%; width: ${right - left}%;`);
-  }),
+  }
 
-  isMinimum: computed("leftPercent", "min", function() {
-    return this.get("leftPercent") === this.get("min");
-  }),
-
-  stepValidator(value) {
-    let step = parseFloat(this.get("step"), 10);
-    return parseFloat((Math.round(value / step) * step).toFixed(1), 10);
-  },
-
-  getClosestState(value) {
-    const fromDistance = Math.abs(this.get("from") - value);
-    const toDistance = Math.abs(this.get("to") - value);
-    if (toDistance < fromDistance) {
-      return "to";
-    } else {
-      return "from";
-    }
-  },
+  get isMinimum() {
+    return this.leftPercent === this.min;
+  }
 
   setValueFromEvent(value) {
     let states = {
-      from: this.get("from"),
-      to: this.get("to")
+      from: this.from,
+      to: this.to,
     };
 
     let exactVal = this.percentToValue(this.positionToPercent(value));
@@ -69,4 +70,14 @@ export default Component.extend(FocusableMixin, ColorMixin, {
     states[this.getClosestState(closestVal)] = closestVal;
     this.get("onChange")(states);
   }
-});
+
+  getClosestState(value) {
+    const fromDistance = Math.abs(this.from - value);
+    const toDistance = Math.abs(this.to - value);
+    if (toDistance < fromDistance) {
+      return "to";
+    } else {
+      return "from";
+    }
+  }
+}
